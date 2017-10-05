32440912c4e5844a60c9fbdd0ce53cf098cc7e6d
amarinas <ali.marinas@icloud.com>
2017-10-04 23:37:02 -0700

    initial

---
+++ quiz.js
@@ -0,0 +1,9 @@
+let answer = prompt('What programming language is the name of a gem?');
+
+if (answer.toLowerCase() === 'ruby' ){
+    document.write("<p>Thats right!</p>");
+
+}else{
+    document.write("<p>try Again!</p>");
+
+}
