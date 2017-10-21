60054d1977ce716a6870c6c7a64e511df084b873
amarinas <ali.marinas@icloud.com>
2017-10-20 23:48:53 -0700

    update

--- javascript/loops/script.js
+++ javascript/loops/script.js
@@ -1,9 +1,15 @@
-function randomNumber(upper) {
+let upper = 10000;
+let randomNumber = getRandomNumber(upper);
+let guess;
+let attempts = 0;
+
+function getRandomNumber(upper) {
   return Math.floor( Math.random() * upper ) + 1;
 }
 
-let counter = 0;
-while (counter <= 10 ) {
-    document.write("<p>Now in loop #" + counter + "</p>");
-    counter += 1;
+while (guess !== randomNumber) {
+    guess = getRandomNumber(upper);
+    attempts += 1;
 }
+document.write("<p> The random number was "  + randomNumber + " </p>");
+document.write("<p> It took the computer " + attempts + " attemps to guess the number</p>");
