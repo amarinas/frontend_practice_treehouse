0b780f4486ca6b3dcc117a8383b0bedc7e59dc02
amarinas <ali.marinas@icloud.com>
2017-10-21 00:40:56 -0700

    review

---
+++ javascript/challenge1/js/script.js
@@ -0,0 +1,27 @@
+var html = '';
+var rgbColor;
+
+function randomRGB(){
+  return Math.floor(Math.random() * 256);
+}
+
+function randomColor(){
+  var color = 'rgb(';
+  color +=randomRGB() + ',';
+  color +=randomRGB() + ',';
+  color +=randomRGB() + ')';
+  return color;
+
+}
+
+function print(message){
+  document.write(message);
+}
+
+for(var i=1; i<=10; i++){
+rgbColor = randomColor();
+html += '<div style="background-color:' + rgbColor + '"></div>';
+
+}
+
+print(html);
