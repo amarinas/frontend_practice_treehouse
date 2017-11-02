13681edc8159e4382704f9dce66d690f02f72e69
amarinas <ali.marinas@icloud.com>
2017-11-01 23:37:20 -0700

    pet adoption site

---
+++ jQuery/Petsite/app.js
@@ -0,0 +1,31 @@
+$('.loc').hover(
+  function(){
+    $(this).html("<strong>Location:</strong> Your house?!");
+  },
+  function() {
+    $(this).html("<strong>Location:</strong> Treehouse Adoption Center");
+
+});
+  //Add a pet to the page with user input
+  $('#add-pet').on('click', function(){
+    
+    // grab info from the form
+    var $name = $('#pet-name');
+    var $species = $('#pet-species');
+    var $notes = $('#pet-notes');
+
+    //Assemble the HTML of our new element with the above variables
+
+    var $newPet = $(
+      '<section class= "six columns"><div class="card"><p><strong>Name:</strong> ' + $name.val() +
+      '</p><p><strong>Species:</strong> ' + $species.val() +
+      '</p><p><strong>Notes:</strong> ' + $notes.val() +
+      '</p><span class="close">&times;</span></div></section>'
+    );
+
+    //Attach the new element to the page
+
+    $('#posted-pets').append($newPet);
+
+
+  });
