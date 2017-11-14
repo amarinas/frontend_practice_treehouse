e8f641b21b17fe49085d686a8f2c0e09403ebe74
amarinas <ali.marinas@icloud.com>
2017-11-14 00:03:03 -0800

    change to jquery for the assignment

---  Ajax/usernotification/js/widget.js
+++  Ajax/usernotification/js/widget.js
@@ -1,42 +1,76 @@
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function(){
-  if(xhr.readyState ===4){
-    var employees = JSON.parse(xhr.responseText);
-    var statusHTML = '<ul class="bulleted">';
-    for (var i=0; i < employees.length; i++){
-      if(employees[i].inoffice === true){
-        statusHTML += '<li class="in">';
-    }else{
-      statusHTML += '<li class="out">';
-    }
-    statusHTML += employees[i].name;
-    statusHTML += '</li>';
-    }
-    statusHTML += '</ul>';
-    document.getElementById('employeeList').innerHTML = statusHTML;
-  }
-};
+// var xhr = new XMLHttpRequest();
+// xhr.onreadystatechange = function(){
+//   if(xhr.readyState ===4){
+//     var employees = JSON.parse(xhr.responseText);
+//     var statusHTML = '<ul class="bulleted">';
+//     for (var i=0; i < employees.length; i++){
+//       if(employees[i].inoffice === true){
+//         statusHTML += '<li class="in">';
+//     }else{
+//       statusHTML += '<li class="out">';
+//     }
+//     statusHTML += employees[i].name;
+//     statusHTML += '</li>';
+//     }
+//     statusHTML += '</ul>';
+//     document.getElementById('employeeList').innerHTML = statusHTML;
+//   }
+// };
 
-xhr.open('GET', 'data/employees.json');
-xhr.send();
-
-var roomRequest = new XMLHttpRequest();
-roomRequest.onreadystatechange = function () {
-  if(roomRequest.readyState === 4 && roomRequest.status === 200) {
-    var rooms = JSON.parse(roomRequest.responseText);
-    var statusHTML = '<ul class="rooms">';
-    for (var i=0; i<rooms.length; i += 1) {
-      if (rooms[i].available === true) {
-        statusHTML += '<li class="empty">';
-      } else {
-        statusHTML += '<li class="full">';
-      }
-      statusHTML += rooms[i].room;
-      statusHTML += '</li>';
-    }
+$(document).ready(function (){
+  var url="../data/employees.json";
+  $.getJSON(url, function (response){
+       var statusHTML = '<ul class = "bulleted">';
+       $.each(response, function(index, employee){
+         if (employee.inoffice === true){
+             statusHTML += '<li class = "in">';
+         } else {
+             statusHTML += '<li class = "out">';
+         }
+    statusHTML += employee.name +'</li>';
+    });
     statusHTML += '</ul>';
-    document.getElementById('roomList').innerHTML = statusHTML;
-  }
-};
-roomRequest.open('GET', '../data/rooms.json');
-roomRequest.send();
+    $('#employeeList').html(statusHTML);
+  }); //end json
+}); //end ready
+
+// xhr.open('GET', 'data/employees.json');
+// xhr.send();
+
+// var roomRequest = new XMLHttpRequest();
+// roomRequest.onreadystatechange = function () {
+//   if(roomRequest.readyState === 4 && roomRequest.status === 200) {
+//     var rooms = JSON.parse(roomRequest.responseText);
+//     var statusHTML = '<ul class="rooms">';
+//     for (var i=0; i<rooms.length; i += 1) {
+//       if (rooms[i].available === true) {
+//         statusHTML += '<li class="empty">';
+//       } else {
+//         statusHTML += '<li class="full">';
+//       }
+//       statusHTML += rooms[i].room;
+//       statusHTML += '</li>';
+//     }
+//     statusHTML += '</ul>';
+//     document.getElementById('roomList').innerHTML = statusHTML;
+//   }
+// };
+// roomRequest.open('GET', '../data/rooms.json');
+// roomRequest.send();
+
+$(document).ready(function (){
+  var url="../data/rooms.json";
+  $.getJSON(url, function (response){
+       var statusHTML = '<ul class = "rooms">';
+       $.each(response, function(index, rooms){
+         if (rooms.available === true){
+             statusHTML += '<li class = "empty">';
+         } else {
+             statusHTML += '<li class = "full">';
+         }
+    statusHTML += rooms.room +'</li>';
+    });
+    statusHTML += '</ul>';
+    $('#roomList').html(statusHTML);
+  }); //end json
+}); //end ready
