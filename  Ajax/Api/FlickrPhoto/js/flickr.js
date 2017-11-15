229f1bfcf52df62a54fa6af7f4aa68b4191dec35
amarinas <ali.marinas@icloud.com>
2017-11-14 23:46:28 -0800

    created a flickr api call

---
+++  Ajax/Api/FlickrPhoto/js/flickr.js
@@ -0,0 +1,25 @@
+$(document).ready(function (){
+  $('button').click(function (){
+    $('button').removeClass("selected");
+    $(this).addClass("selected");
+    var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+    var animal = $(this).text();
+    var flickrOptions = {
+                          tags: animal,
+                          format: "json"
+    };
+    function display(data){
+      var photoHTML = '<ul>';
+      $.each(data.items, function(i, photo){
+        photoHTML += '<li class="grid-25 tablet-grid-50">';
+        photoHTML += '<a href="' + photo.link + '" class = "image">';
+        photoHTML += '<img src = "' + photo.media.m +  '"></a> </li>';
+      });
+      photoHTML += '</ul>';
+      $('#photos').html(photoHTML);
+      
+    }
+    $.getJSON(flickrAPI, flickrOptions, display);
+  });
+  
+}); //end ready
