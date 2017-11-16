b1c4b662cd9d02195c64f951cf136d255dc69e52
amarinas <ali.marinas@icloud.com>
2017-11-16 00:01:37 -0800

    calculator using object

---
+++ OOP/javascript/Calculator/calculator.js
@@ -0,0 +1,14 @@
+var calculator = {
+		sum: 0,
+		add: function(value) {
+      this.sum += value;
+
+    },
+    clear: function() {
+      this.sum = 0
+
+    }, 
+    equals: function() {
+      return this.sum
+    }
+}
--- OOP/javascript/dice/dice.js
+++ OOP/javascript/dice/dice.js
@@ -2,11 +2,9 @@
 var dice = {
   sides: 6,
   roll: function () {
-  
+
   var randomNumber = Math.floor(Math.random() * this.sides) + 1;
   return randomNumber;
  }
 
 }
-
-
