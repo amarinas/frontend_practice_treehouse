655102fd1807814872dfb6addc784e247ba6eb56
amarinas <ali.marinas@icloud.com>
2017-11-16 00:07:51 -0800

    change dice.js into a constructor

--- OOP/javascript/dice/dice.js
+++ OOP/javascript/dice/dice.js
@@ -1,10 +1,13 @@
 
-var dice = {
-  sides: 6,
-  roll: function () {
+function Dice(sides){
+  this.sides= sides,
+  this.roll= function () {
 
   var randomNumber = Math.floor(Math.random() * this.sides) + 1;
   return randomNumber;
  }
 
 }
+
+
+var dice = new Dice(6);
