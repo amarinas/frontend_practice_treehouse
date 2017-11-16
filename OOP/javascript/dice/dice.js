7d6a311c8713d9dd0d31a2723b6406eec172fde5
amarinas <ali.marinas@icloud.com>
2017-11-16 00:28:36 -0800

    added a prototype to the dice.js

--- OOP/javascript/dice/dice.js
+++ OOP/javascript/dice/dice.js
@@ -1,13 +1,29 @@
+//
+// function Dice(sides){
+//   this.sides= sides,
+//   this.roll= function () {
+//
+//   var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+//   return randomNumber;
+//  }
+// }
+//
+//
+// var dice = new Dice(6);
 
 function Dice(sides){
-  this.sides= sides,
-  this.roll= function () {
-
-  var randomNumber = Math.floor(Math.random() * this.sides) + 1;
-  return randomNumber;
- }
+  this.sides = sides;
 
 }
 
+Dice.prototype.roll = function(){
+    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+    return randomNumber;
 
+}
+
+//
 var dice = new Dice(6);
+var dice10 = new Dice(10)
+
+console.log(dice.roll === dice10.roll);
